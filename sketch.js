a73037a7147676542f2361f253858693e87a4238
commanderkaua <103076194+commanderkaua@users.noreply.github.com>
2022-05-10 19:39:04 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,13 +9,17 @@ var pontos = 0;
 var JOGANDO = 1;
 var MORREU = 0;
 var estado;
-var cactosgroup;
-var nuvensgroup;
+var grupocactos;
+var gruponuvens;
 var gameOver;
-var gameOver_Img;
+var gameOverimg;
 var restart;
-var restart_Img;
- 
+var restartimg;
+var trexmortoanimation;
+var checkpoint;
+var die;
+var jump;
+
 estado = JOGANDO;
 
 
@@ -29,21 +33,27 @@ function preload(){
   cactoImg4 = loadImage("obstacle4.png");
   cactoImg5 = loadImage("obstacle5.png");
   cactoImg6 = loadImage("obstacle6.png");
-  gameOver_Img = loadImage("gameOver.png");
-  restart_Img = loadImage("restart (1).png");
+  gameOverimg = loadImage("gameOver.png");
+  restartimg = loadImage("restart.png");
+  trexmortoanimation = loadAnimation("trex_collided.png");
+  jumpsound = loadSound("jump.mp3");
+  diesound = loadSound("die.mp3");
+  checkPointsound = loadSound("checkPoint.mp3");
 }
 
 function setup() {
   createCanvas(600, 200);
   
-  
-  cactosgroup = new Group();
-  nuvensgroup = new Group();
+  grupocactos = new Group();
+  gruponuvens = new Group();
 
   //crie um sprite de trex
   trex = createSprite(50,180,20,50);
   trex.addAnimation("correndo", trex_correndo);
+  trex.addAnimation("morto",trexmortoanimation);
   trex.depth = 2;
+  trex.debug = false;
+  trex.setCollider("circle",0,0,40);
   
   //adicione dimensão e posição ao trex
   trex.scale = 0.5;
@@ -53,19 +63,22 @@ function setup() {
   chao = createSprite(200,180,400,20);
   chao.addImage("chao que mexe", chao_imagem);
   chao.x = chao.width/2;
-  chao.velocityX = -3;
 
   chao2 = createSprite(200, 193, 400, 10);
   chao2.shapeColor = "white";
 
   //chão fica invisível
   chao2.visible = false;
-  gameOver = createSprite(300,100,20,20);
-  restart = createSprite(300,140,20,30);
-  gameOver.addImage(gameOver_Img);
-  restart.addImage(restart_Img);
-  gameOver.visible = false;
+  
+  restart = createSprite(300,140,50,50);
+  restart.addImage(restartimg);
   restart.visible = false;
+  restart.scale = 0.5
+
+  gameOver = createSprite(300,100,50,50);
+  gameOver.addImage(gameOverimg);
+  gameOver.visible = false;
+  gameOver.scale = 0.5
 }
 
 function draw() {
@@ -79,39 +92,43 @@ function draw() {
 
   text("pontuação:"+pontos,450,30)
 
-    if(estado==JOGANDO){
-      in_Game();
-      if(trex.isTouching(cactosgroup)){
-        estado = MORREU;
+  if(estado==JOGANDO){
+   in_Game();
+   if(trex.isTouching(grupocactos)){
+     estado=MORREU;
+     diesound.play();
+   }
+   
 
-      }
-    }
-  if(estado==MORREU){ 
-      Morte();
-    
   }
+  if(estado==MORREU){
+    Morte();
+
   }
+  
+}
 
 
 
 function pular(){
   if(keyDown("space") && trex.isTouching(chao)) {
     trex.velocityY = -13;
+    jumpsound.play();
   }
   
   trex.velocityY = trex.velocityY + 0.8
   
-  }
+}
 
-  function corrige_chao(){
-    if(chao.x<0){
+function corrige_chao(){
+  if(chao.x<0){
     chao.x = chao.width/2;
-    }
   }
+}
 
 function corrige_chao2(){
   if(chao2.x<0){
-     chao2.x = chao2.width/2;
+    chao2.x = chao2.width/2;
   }
 }
 
@@ -122,17 +139,18 @@ function criar_nuvens(){
     nuvem.addImage("passando",nuvem_imagem); 
     nuvem.scale = 1;
     nuvem.depth = 1;
-    nuvem.lifetime = 700;
-    nuvensgroup.add(nuvem);
+    nuvem.lifetime = 400;
+    gruponuvens.add(nuvem);
   }
 }
+
 function criar_cactos(){
-  if(frameCount%440===0){
+  if(frameCount%80===0){
     cacto = createSprite(650,170,10,10);
-    cacto.velocityX = -3;
+    cacto.velocityX = -(6+pontos/700);
     cacto.scale = 0.5;
     cacto.depth = 1;
-    cacto.lifetime = 700;
+    cacto.lifetime = 300;
 
     switch(Math.round(random(1,6))){
       case 1:
@@ -163,31 +181,54 @@ function criar_cactos(){
       default:
         break;
     }
-    cactosgroup.add(cacto);
+    grupocactos.add(cacto);
   }
 }
 
-  function in_Game(){
+function in_Game(){
 
-    corrige_chao();
-    pular();
-    criar_cactos();
-    criar_nuvens();
-    pontuacao();
+  corrige_chao();
+  pular();
+  criar_cactos();
+  criar_nuvens();
+  pontuacao();
+  chao.velocityX = -(6+pontos/700);
+}
 
+function Morte(){
+  chao.velocityX=0;
+  gameOver.visible = true;
+  restart.visible = true;
+  grupocactos.setVelocityXEach(0);
+  gruponuvens.setVelocityXEach(0);
+  trex.velocityY=0;
+  grupocactos.setLifetimeEach(-1);
+  gruponuvens.setLifetimeEach(-1);
+  trex.changeAnimation("morto",trexmortoanimation);
 
+  if(mousePressedOver(restart)){
+    reiniciar();
   }
-  function Morte(){
-    chao.velocityX=0;
-    gameOver.visible = true;
-    restart.visible = true;
-    cactosgroup.setVelocityXEach(0);
-    nuvensgroup.setVelocityXEach(0);
-  }
+}
 
-  function pontuacao(){
-    pontos=pontos+round(frameCount/60);
+function pontuacao(){
+  pontos=pontos+round(getFrameRate()/60);
+if(pontos%1000==0 && pontos>0){
 
+checkPointsound.play();
 
-  }
+}
+
+}
+function reiniciar(){
+pontos = 0;
+grupocactos.setVelocityXEach(-(4+3*pontos/1000));
+gruponuvens.setVelocityXEach(-2);
+estado = JOGANDO;
+grupocactos.destroyEach();
+gruponuvens.destroyEach();
+trex.changeAnimation("correndo", trex_correndo);
+restart.visible = false;
+gameOver.visible = false;
+}
 
